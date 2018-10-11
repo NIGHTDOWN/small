da56883ca5ae229724b13e5b82336414a067a8fb
xiaohei <2366731874@qq.com>
2018-10-11 14:34:05 +0800

    视频后台

--- application/admin/controller/video/Video.php
+++ application/admin/controller/video/Video.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller\video;
 
 use app\common\controller\Backend;
+use think\Db;
 
 /**
  * 
@@ -31,5 +32,129 @@ class Video extends Backend
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
     
+    /**
+     * 查看
+     */
+    public function index()
+    {
+        //设置过滤方法
+        $this->request->filter(['strip_tags']);
+        if ($this->request->isAjax()) {
+            //如果发送的来源是Selectpage，则转发到Selectpage
+            if ($this->request->request('keyField')) {
+                return $this->selectpage();
+            }
+            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+            $total = $this->model
+                ->where($where)
+                ->order($sort, $order)
+                ->count();
+
+            $list = $this->model
+                ->where($where)
+                ->order($sort, $order)
+                ->limit($offset, $limit)
+                ->select();
+
+            $list = collection($list)->toArray();
+            $categoryList = Db::name('category')->where(['status' => 1])->column('name', 'id');
+            
+            foreach ($list as $key => $value) {
+                $list[$key]['status_text'] = $this->model::STATUSTEXT[$value['status']];
+                $list[$key]['category_text'] = !isset($categoryList[$value['category_id']]) ? '' : $categoryList[$value['category_id']];
+            }
+            $result = array("total" => $total, "rows" => $list);
+
+            return json($result);
+        }
+        return $this->view->fetch();
+    }
+
+    /**
+     * 删除
+     */
+    public function del($ids = "")
+    {
+        // if ($ids) {
+        //     $pk = $this->model->getPk();
+        //     $adminIds = $this->getDataLimitAdminIds();
+        //     if (is_array($adminIds)) {
+        //         $count = $this->model->where($this->dataLimitField, 'in', $adminIds);
+        //     }
+        //     $list = $this->model->where($pk, 'in', $ids)->select();
+        //     $count = 0;
+        //     foreach ($list as $k => $v) {
+        //         $count += $v->delete();
+        //     }
+        //     if ($count) {
+        //         $this->success();
+        //     } else {
+        //         $this->error(__('No rows were deleted'));
+        //     }
+        // }
+        // $this->error(__('Parameter %s can not be empty', 'ids'));
+        $this->error(__('滚犊子吧，删什么删'));
+    }
+
+    /**
+     * 设置视频分类
+     * 
+     * @param string $ids 视频ID
+     */
+    public function set_category($ids = "")
+    {
+        if ($ids) {
+            $row = Db::name('video')->where(['id' => $ids])->find();
+            if ($this->request->isPost()) {
+                $params = $this->request->post("row/a");
+                if (!isset($params['category_id']) || !is_numeric($params['category_id'])) {
+                    $this->error(__('参数错误'));
+                }
+                $data = [
+                    'update_time' => time(),
+                    'category_id' => $params['category_id']
+                ];
+                Db::name('video')->where(['id' => $row['id']])->update($data);
+                // 推荐操作
+                // if ($row['recommend']){
+                //     if ($row['category_id']!=$params['category_id']){
+                //         self::delTopVideoFromCache($params['video_id'],$row['category_id']);
+                //     }
+                //     self::addTopVideoToCache($params['video_id'],$params['category_id']);
+                // }
+                $this->success();
+            }
+
+            $categoryList = Db::name('category')->where(['status' => 1])->select();
+            $this->view->assign("row", $row);
+            $this->view->assign("category_list", $categoryList);
+            return $this->view->fetch();
+        }
+    }
+
+    /**
+     * 审核视频
+     * 
+     * @param  string $ids [description]
+     * @return [type]      [description]
+     */
+    public function check_video($ids = "")
+    {
+        if ($ids) {
+            $row = Db::name('video')->where(['id' => $ids])->find();
+            if ($this->request->isPost()) {
+                $params = $this->request->post("row/a");
+                $result = $this->model->check_video($row['id'], $params['stauts'], $params['remark']);
+                if ($result) {
+                    $this->success();
+                } else {
+                    $this->error($this->model->getError());
+                }
+            }
+
+            $this->view->assign("row", $row);
+            return $this->view->fetch();
+        }
+    }
 
 }
--- application/admin/model/Video.php
+++ application/admin/model/Video.php
@@ -22,12 +22,53 @@ class Video extends Model
         'create_time_text',
         'update_time_text'
     ];
+
+    public static $status = [
+        'DELETE' => -1,
+        'HIDE' => 0,
+        'DISPLAY' => 1,
+        'ROBOT_FAILD' => 2,
+        'VIOLATION' => 3,
+        'FROZEN' => 5,
+        'COMPLAIN' => 6,
+        'APPEALING' => 7,
+        'ROBOT_SUCCESS' => 8,
+        'CHECK_NO_PASS' => 9,
+        'DRAFT' => 10,
+    ];
     
+    const STATUSTEXT = [
+        -1 => '删除',
+        0 => '未发布',
+        1 => '已发布',
+        2 => '机器审核未通过',
+        3 => '违规', //人工巡查 用户举报
+        5 => '冻结中', //原始视频被删除 或者违规了 (相对于转载的视频)
+        6 => '用户申诉',
+        7 => '用户申诉',
+        8 => '机器审核通过',
+        9 => '审核不通过',
+        10 => '草稿',
+    ];
 
     
+    public function check_video($video_id, $status, $remark)
+    {
+        // 第三方相关操作没写
+        $data = [
+           'update_time'=>time()
+        ];
 
+        if ($status == 1) {
+            $data['status'] = self::$status['DISPLAY'];
+        } else {
+            $data['status'] = self::$status['CHECK_NO_PASS'];
+        }
 
-
+        $this->where(['id' => $video_id])->save($date);
+        return true;
+    }
+    
     public function getProcessDoneTimeTextAttr($value, $data)
     {
         $value = $value ? $value : (isset($data['process_done_time']) ? $data['process_done_time'] : '');

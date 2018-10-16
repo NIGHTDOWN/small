cff879f69d379bce773b1aad502fb7c92cc974d7
fir <3024452976@qq.com>
2018-10-16 18:09:22 +0800

    后台.举报管理 新增

---
+++ application/admin/model/ContentReport.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+class ContentReport extends Model
+{
+    // 表名
+    protected $name = 'content_report';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = false;
+
+    // 定义时间戳字段名
+    protected $createTime = false;
+    protected $updateTime = false;
+    
+    // 追加属性
+    protected $append = [
+        'type_text',
+        'admin_time_text',
+        'create_time_text',
+        'status_text'
+    ];
+    
+
+    
+    public function getTypeList()
+    {
+        return ['0' => __('Type 0'),'1' => __('Type 1'),'2' => __('Type 2')];
+    }     
+
+    public function getStatusList()
+    {
+        return ['0' => __('Status 0'),'1' => __('Status 1')];
+    }     
+
+
+    public function getTypeTextAttr($value, $data)
+    {        
+        $value = $value ? $value : (isset($data['type']) ? $data['type'] : '');
+        $list = $this->getTypeList();
+        return isset($list[$value]) ? $list[$value] : '';
+    }
+
+
+    public function getAdminTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['admin_time']) ? $data['admin_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+
+    public function getCreateTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['create_time']) ? $data['create_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+
+    public function getStatusTextAttr($value, $data)
+    {        
+        $value = $value ? $value : (isset($data['status']) ? $data['status'] : '');
+        $list = $this->getStatusList();
+        return isset($list[$value]) ? $list[$value] : '';
+    }
+
+    protected function setAdminTimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+    protected function setCreateTimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+
+}

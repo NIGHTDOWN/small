e95238057fdcea5d0e61e92f1a9747ecc4d1823a
fir <3024452976@qq.com>
2018-10-25 17:30:57 +0800

    修改

--- public/assets/js/backend/activity/top.js
+++ public/assets/js/backend/activity/top.js
@@ -18,26 +18,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'template'], function
 
             // 为表格绑定事件
             Table.api.bindevent(table);
-
-            // 查看视频
-            $(document).on('click', ".show-top-video", function (data) {
-                var url = $(this).attr('data-url');
-                if(!url) return false;
-                var msg = $(this).attr('data-title');
-                // var width = $(this).attr('data-width');
-                // var height = $(this).attr('data-height');
-                // var area = [$(window).width() > 800 ? (width?width:'800px') : '95%', $(window).height() > 600 ? (height?height:'600px') : '95%'];
-                //var area = ['90%', '100%'];
-                var options = {
-                    shadeClose: true,
-                    shade: [0.3, '#393D49'],
-                    //area: area,
-                    callback:function(value){
-                        // CallBackFun(value.id, value.name);//在回调函数里可以调用你的业务代码实现前端的各种逻辑和效果
-                    }
-                };
-                Fast.api.open(url,msg,options);
-            });
         },
         add: function () {
             Controller.api.bindevent();

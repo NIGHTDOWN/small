b250da20e7d7b24cbeb0c2f056b8546cd0ead037
fir <3024452976@qq.com>
2018-10-25 17:22:54 +0800

    后台.活动管理 增加查看视频列表

---
+++ application/admin/model/ActivityTopData.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\admin\model;
+
+use think\Model;
+
+class ActivityTopData extends Model
+{
+    // 表名
+    protected $name = 'activity_top_data';
+    
+    // 自动写入时间戳字段
+    protected $autoWriteTimestamp = false;
+
+    // 定义时间戳字段名
+    protected $createTime = false;
+    protected $updateTime = false;
+    
+    // 追加属性
+    protected $append = [
+        'create_time_text',
+        'update_time_text',
+        'start_time_text',
+        'end_time_text'
+    ];
+    
+
+    
+
+
+
+    public function getCreateTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['create_time']) ? $data['create_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+
+    public function getUpdateTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['update_time']) ? $data['update_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+
+    public function getStartTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['start_time']) ? $data['start_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+
+    public function getEndTimeTextAttr($value, $data)
+    {
+        $value = $value ? $value : (isset($data['end_time']) ? $data['end_time'] : '');
+        return is_numeric($value) ? date("Y-m-d H:i:s", $value) : $value;
+    }
+
+    protected function setCreateTimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+    protected function setUpdateTimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+    protected function setStartTimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+    protected function setEndTimeAttr($value)
+    {
+        return $value && !is_numeric($value) ? strtotime($value) : $value;
+    }
+
+    public function video()
+    {
+        return $this->belongsTo('Video','video_id','id',[],'left')->setEagerlyType(0);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('User','user_id','id',[],'left')->setEagerlyType(1);
+    }
+
+}

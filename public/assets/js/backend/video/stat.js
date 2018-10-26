60b0ff7763f07d8a766260d04afe212d8b6a4d08
fir <3024452976@qq.com>
2018-10-26 20:15:48 +0800

    后台.视频统计 修改

--- public/assets/js/backend/video/stat.js
+++ public/assets/js/backend/video/stat.js
@@ -1,114 +1,74 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'echarts', 'echarts-theme'], function ($, undefined, Backend, Table, Form, Echarts) {
+define(['jquery', 'bootstrap', 'backend', 'table', 'form', 'echarts', 'echarts-theme'], function ($, undefined, Backend, Table, Form, Echarts, undefined) {
 
     var Controller = {
         index: function () {
-            // 初始化表格参数配置
-            Table.api.init({
-                extend: {
-                    index_url: 'video/stat/index',
-                    add_url: '',
-                    edit_url: '',
-                    del_url: '',
-                    multi_url: '',
-                    table: 'video',
-                    echart_url: 'video/stat/chartdata'
-                }
+            // 基于准备好的dom，初始化echarts实例
+            var myChart = Echarts.init(document.getElementById('echart'), 'walden');
+
+            // 指定图表的配置项和数据
+            var option = {
+                title: {
+                    text: '',
+                    subtext: ''
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {},
+                toolbox: {
+                    show: false,
+                    feature: {
+                        magicType: {show: true, type: ['stack', 'tiled']},
+                        saveAsImage: {show: true}
+                    }
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: Config.stat.columns
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                grid: [{
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                }],
+                series: Config.stat.data
+            };
+
+            // 使用刚指定的配置项和数据显示图表。
+            myChart.setOption(option);
+
+            $(window).resize(function () {
+                myChart.resize();
             });
 
+            Form.api.bindevent($("form[role=form]"));
 
-            // 图表表格初始化
-            var echart = $('#echart');
-            echart.bootstrapTable({
-                url: $.fn.bootstrapTable.defaults.extend.echart_url,
-                // searchFormVisible: false,
-                showToggle: false,
-                showColumns: false,
-                showExport: false,
-                // commonSearch: false,
-                commonSearch: true,
-                searchFormVisible: true,
-                columns: [
-                    [
-                        {field: 'date_range', title: __('日期'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'category_id', title: __('分类'),column:'category_id',addclass:'selectpage', data:'data-source="category/selectpage"  data-params="{\'custom[type]\':\'video\'}"  data-field="name"'}
-                    ]
-                ]
-            });
-
-            echart.on('post-body.bs.table', function (e, settings, json, xhr) {
-                //初始化
-                var myChart = Echarts.init(document.getElementById('echart'), 'walden');
-
-                //组装数据
-                var series_list = [];
-                Object.keys(settings.data).forEach(function(key){
-                    var value=settings.data[key];
-                    series_list.push({
-                        name:value.name,
-                        type:'line',
-                        data: value.list,
-                        showAllSymbol:true
-                    })
+            $(".datetimerange").on("blur", function () {
+                Fast.api.ajax({
+                    url: 'video/stat/index',
+                    data: {date: $(this).val()}
+                }, function (data) {
+                    myChart.setOption({
+                        xAxis: {
+                            data: data.columns
+                        },
+                        series: data.data
+                    });
+                    return false;
                 });
-
-                //参数配置
-                var option = {
-                    title: {
-                        text: '视频统计'
-                    },
-                    tooltip: {
-                        trigger: 'axis',
-                        axisPointer: {
-                            type: 'cross'
-                        },
-                        backgroundColor: 'rgba(245, 245, 245, 0.8)',
-                        borderWidth: 1,
-                        borderColor: '#ccc',
-                        padding: 10,
-                        textStyle: {
-                            color: '#000'
-                        },
-                        position: function (pos, params, el, elRect, size) {
-                            var obj = {top: 10};
-                            obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 30;
-                            return obj;
-                        },
-                        extraCssText: 'width: 170px'
-                    },
-
-                    legend: {
-                        data:['']
-                    },
-                    xAxis: {
-                        type:'category',
-                        data: settings.date_list
-                    },
-                    brush: {
-                        xAxisIndex: 'all',
-                        brushLink: 'all',
-                        outOfBrush: {
-                            colorAlpha: 0.1
-                        }
-                    },
-                    yAxis: {},
-                    series: series_list
-                };
-                myChart.setOption(option);
             });
-
-            // 为表格绑定事件
-            Table.api.bindevent(echart);
-        },
-        add: function () {
-            Controller.api.bindevent();
-        },
-        edit: function () {
-            Controller.api.bindevent();
-        },
-        api: {
-            bindevent: function () {
-                Form.api.bindevent($("form[role=form]"));
-            }
+            $(document).on("click", ".btn-refresh", function () {
+                $(".datetimerange").trigger("blur");
+            });
+            //每隔一分钟定时刷新图表
+            setInterval(function () {
+                $(".btn-refresh").trigger("click");
+            }, 60000);
         }
     };
     return Controller;

ca2312e4f3ae0215d718d927f2b62a370d18498a
fir <3024452976@qq.com>
2018-10-30 18:27:05 +0800

    后台.短信 修改

--- public/assets/js/backend/message/smstask.js
+++ public/assets/js/backend/message/smstask.js
@@ -5,7 +5,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             // 初始化表格参数配置
             Table.api.init({
                 extend: {
-                    index_url: 'message/smstask/index',
+                    index_url: 'message/sms_task/index',
                     // add_url: 'message/smstask/add',
                     // edit_url: 'message/smstask/edit',
                     // del_url: 'message/smstask/del',
@@ -45,7 +45,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                                         return false;
                                     },
                                     confirm:'确认发送?',
-                                    url: 'message/smstask/send',
+                                    url: 'message/sms_task/send',
                                     success: function (data, ret) {
                                         if (ret.code === 1){
                                             $('.btn-refresh').trigger('click');

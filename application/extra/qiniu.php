011e55ee8e8f83d84c6387fecd17fa4940c3226a
flyn <251165893@qq.com>
2018-10-31 10:25:11 +0800

    update

---
+++ application/extra/qiniu.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/1/29
+ * Time: 8:58
+ */
+return [
+    'ak' => 'OUahOqyuJIzcOT_ImZl4tfg1MyH2X2mVl8Mwk6GF',
+    'sk' => '_iQedrZ1zSls6Fs0RmX94ZQ6QdAHekivaXO-eHKk',
+    //公开视频空间
+    'public_video_bkt' => 'videopub',
+    'public_video_bkt_domain' => 'videopub.actuive.com',
+    'public_video_bkt_protocol' => 'https',
+    //私有加密视频空间
+ /*   'private_video_bkt' => 'videopri',
+    'private_video_bkt_domain' => 'videopri.yixiu08.com',
+    'private_video_bkt_protocol' => 'https',*/
+
+    'private_video_bkt' => 'videopri1',
+    'private_video_bkt_domain' => 'pa1ooso5l.bkt.clouddn.com',
+    'private_video_bkt_protocol' => 'http',
+
+    //截图空间
+    'screenshot_bkt' => 'testscreenshot',
+    'screenshot_bkt_domain' => 'p5knudqvz.bkt.clouddn.com',
+    'screenshot_bkt_protocol' => 'http',
+    'screenshot_source_domain' => 'p5knudqvz.com0.z0.glb.clouddn.com',
+
+    //原视频空间
+    'original_video_bkt' => 'videoori',
+    'original_video_bkt_domain' => 'p5kmv2o0x.bkt.clouddn.com',
+    'original_video_bkt_protocol' => 'http',
+
+    //头像空间
+    'headimg_bkt' => 'testheadimg',
+    'headimg_bkt_domain' => 'p5z1781je.bkt.clouddn.com',
+    'headimg_bkt_protocol' => 'http',
+
+
+    //音乐空间
+    'music_bkt' => 'musicpub',
+    'music_bkt_domain' => 'p3giatxrd.bkt.clouddn.com',
+    'music_bkt_protocol' => 'http',
+
+    //封面图片空间
+    'cover_img_bkt' => 'testscreenshot',
+    'cover_img_bkt_domain' => 'p5knudqvz.bkt.clouddn.com',
+    'cover_img_bkt_protocol' => 'http',
+
+    //主题图片空间
+    'subject_img_bkt' => 'testscreenshot',
+    'subject_img_bkt_domain' => 'p5knudqvz.bkt.clouddn.com',
+    'subject_img_bkt_protocol' => 'http',
+
+    //广告图片空间
+    'ad_img_bkt' => 'testscreenshot',
+    'ad_img_bkt_domain' => 'p5knudqvz.bkt.clouddn.com',
+    'ad_img_bkt_protocol' => 'http',
+
+    //其他图片空间
+    'other_image_bkt' => 'testheadimg',
+    'other_image_bkt_domain' => 'p5z1781je.bkt.clouddn.com',
+    'other_image_bkt_protocol' => 'http',
+
+    //临时存放加密空间(例如视频自定义封面违禁后转存到此空间)
+    'protected_temp_bkt' => 'testheadimg',
+    'protected_temp_bkt_domain' => 'p5z1781je.bkt.clouddn.com',
+    'protected_temp_bkt_protocol' => 'http',
+
+
+    //主页轮播图空间
+    'site_banner_bkt' => 'testheadimg',
+    'site_banner_domain' => 'p5z1781je.bkt.clouddn.com',
+    'site_banner_protocol' => 'http',
+
+    //主页轮播图空间
+    'id_card_bkt' => 'testidcard',
+    'id_card_domain' => 'idcard.yixiu08.com',
+    'id_card_protocol' => 'http',
+
+
+    //水印图片
+    'watermark_picture' => 'http://p79t47c8b.com0.z0.glb.clouddn.com/video_water_mark_2018_5_18.png',
+];
